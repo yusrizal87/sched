4e8addd161f66b305ee9c78b3c11c335c369c6e8
yusrizal87 <myusrizal@student.ciputra.ac.ic>
2022-12-11 22:22:51 +0700

    Initial commit

---
+++ app/src/main/java/com/uc/sched/calendar.kt
@@ -0,0 +1,11 @@
+package com.uc.sched
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class calendar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
---
+++ app/src/main/java/com/uc/sched/signupactvity.kt
@@ -0,0 +1,11 @@
+package com.uc.sched
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signupactvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signupactvity)
+    }
+}
